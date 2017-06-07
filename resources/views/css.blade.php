721d00a1bb0f7cdf50b28260def4f7c5ccb1ad3b
kopin88 <kopin88@gmail.com>
2017-06-07 19:04:11 +0630

    l-d

---
+++ resources/views/css.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+{{-- <link rel="stylesheet" href="/css/bt.min.css">
+<link rel="stylesheet" href="/css/material-dashboard.css">
+<link rel="stylesheet" href="/css/fonts.css"> --}}
+<!--  Material Dashboard CSS    -->
+{{-- <link href="../css/material-dashboard.css" rel="stylesheet"/> --}}
+{{-- <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet"> --}}
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
