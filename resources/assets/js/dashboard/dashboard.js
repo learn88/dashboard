630919d53000cb3005ad8bb2b3706a4f2b673c98
kopin88 <kopin88@gmail.com>
2017-06-28 23:38:17 +0630

    *

--- resources/assets/js/dashboard/dashboard.js
+++ resources/assets/js/dashboard/dashboard.js
@@ -1,16 +1,41 @@
+require('./material.min');
 
 
-require('./jquery.bootstrap-wizard');
+require('perfect-scrollbar');
+
+require('jquery-validation');
+
+
+// require('./jquery.bootstrap-wizard');
+require('twitter-bootstrap-wizard');
+
+
 require('./bootstrap-notify');
 require('./jquery.sharrre');
-require('./bootstrap-datetimepicker');
-require('./jquery-jvectormap');
-require('./nouislider.min');
+
+
+// require('./bootstrap-datetimepicker');
+require('eonasdan-bootstrap-datetimepicker');
+
+// require('./jquery-jvectormap');
+require('jvectormap-next');
+
+// require('./nouislider.min');
+require('nouislider');
+
 require('./jquery.select-bootstrap');
 require('./jquery.datatables');
-require('./sweetalert2');
-require('./jasny-bootstrap.min');
-require('./fullcalendar.min');
+
+// require('./sweetalert2');
+require('sweetalert2');
+
+// require('./jasny-bootstrap.min');
+require('jasny-bootstrap/dist/js/jasny-bootstrap');
+
+
+// require('./fullcalendar.min');
+require('fullcalendar');
+
 require('./jquery.tagsinput');
 require('./material-dashboard');
-require('./demo');
+// require('./demo');
